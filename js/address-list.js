5783f691f43813957df3eeb493ae790f4521e5d6
EudesGomesJr <eudes_jr@hotmail.com>
2024-06-21 09:24:13 -0300

    inciando a implementação

--- js/address-list.js
+++ js/address-list.js
@@ -1,3 +1,50 @@
 import MenuDashboard from "../js/menu-dashboard.js";
 
 document.getElementById("navArea").innerHTML = MenuDashboard();
+
+// Lê a lista de endereços existente
+let addressStorageList = window.localStorage.getItem('address-list') ?? '[]';
+addressStorageList = JSON.parse(addressStorageList);
+
+let addressHtmlList = document.getElementById('address-html-list');
+
+
+const addTaskHtml = (value) => {
+
+    let tr = document.createElement('tr');
+    
+    // Inclui o ID na tabela
+    let tdid = document.createElement('td');
+    tdid.textContent = (Math.random() * 1000000).toFixed();
+    tr.append(tdid);
+
+    // Inclui os campos trazidos do LocalStorage
+    for (const property in value) {
+      let td = document.createElement('td');
+      td.textContent = value[property];
+      tr.append(td);
+    }
+
+    // Inclui os botões de Editar e Excluir
+    let tdbutton = document.createElement('td');
+    tdbutton.classList.add("btn.acoes");
+
+    let buttonEdit = document.createElement('button');
+    buttonEdit.textContent = "Editar";
+    tdbutton.append(buttonEdit);
+
+    let buttonDelete = document.createElement('button');
+    buttonDelete.textContent = "Excluir";
+    tdbutton.append(buttonDelete);
+
+    tr.append(tdbutton);
+
+    // Adiciona à lista de endereços no HTML
+    addressHtmlList.append(tr);
+  }
+
+// Montar um loop para inserir, em tbody, tantas tags tr (com uma td para cada coluna) quantos sejam os endereços
+
+for (const address of addressStorageList) {
+    addTaskHtml(address);
+};
