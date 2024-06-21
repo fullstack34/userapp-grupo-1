de4179249f146bb1a1512776dac27635844a10a3
EudesGomesJr <eudes_jr@hotmail.com>
2024-06-21 10:13:19 -0300

    listando endereços do localStorage

--- js/address-list.js
+++ js/address-list.js
@@ -8,24 +8,18 @@ addressStorageList = JSON.parse(addressStorageList);
 
 let addressHtmlList = document.getElementById('address-html-list');
 
-
 const addTaskHtml = (value) => {
 
     let tr = document.createElement('tr');
-    
-    // Inclui o ID na tabela
-    let tdid = document.createElement('td');
-    tdid.textContent = (Math.random() * 1000000).toFixed();
-    tr.append(tdid);
-
-    // Inclui os campos trazidos do LocalStorage
+  
+    // Inclui, na tag tr, os campos trazidos do LocalStorage
     for (const property in value) {
       let td = document.createElement('td');
       td.textContent = value[property];
       tr.append(td);
     }
 
-    // Inclui os botões de Editar e Excluir
+    // Inclui, na tag tr, os botões de Editar e Excluir
     let tdbutton = document.createElement('td');
     tdbutton.classList.add("btn.acoes");
 
@@ -39,11 +33,9 @@ const addTaskHtml = (value) => {
 
     tr.append(tdbutton);
 
-    // Adiciona à lista de endereços no HTML
+    // Adiciona a tag tr à lista de endereços no HTML
     addressHtmlList.append(tr);
   }
-
-// Montar um loop para inserir, em tbody, tantas tags tr (com uma td para cada coluna) quantos sejam os endereços
 
 for (const address of addressStorageList) {
     addTaskHtml(address);
